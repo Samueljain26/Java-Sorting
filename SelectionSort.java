6264f01085f8dea465c6357cd1b94b02dd0cdc8d
Samueljain26 <samueljain77@gmail.com>
2025-03-25 10:39:05 +0530

    [Samuel] ADD: Selection Sort.

--- SelectionSort.java
+++ SelectionSort.java
@@ -4,17 +4,17 @@ class SelectionSort {
     // Method to sort
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i;
+            int smallest = i;
 
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j; 
+                if (arr[j] < arr[smallest]) {
+                    smallest = j;
                 }
             }
             // Swap the found minimum element with the first element
             int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+            arr[i] = arr[smallest];
+            arr[smallest] = temp;
         }
 
         // Display sorted array
