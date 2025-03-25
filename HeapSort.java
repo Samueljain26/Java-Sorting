449528633246e04415cc3f1888e995b03fc7c9e2
Samueljain26 <samueljain77@gmail.com>
2025-03-25 09:48:06 +0530

    [Samuel] ADD: Heap Sort.

---
+++ HeapSort.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class HeapSort {
+    // Heap Sort
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+
+        // Display sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    // Method to heapify a subtree rooted at index i
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 6, 1, 3, 8, 4};
+        heapSort(arr);
+    }
+}
+/* 1 2 3 4 5 6 8  */
